ad9b17e9cdbb85038b5d1c194f8dd281e6b96cbb
shay asanova <moeshakasanova@gmail.com>
2025-09-16 13:02:15 +0400

    added functionality to modal project

--- modal-window/script.js
+++ modal-window/script.js
@@ -1,1 +1,28 @@
 'use strict';
+
+const openModalBtns = document.querySelectorAll('.show-modal');
+const modal = document.querySelector('.modal');
+const closeModalBtn = document.querySelector('.close-modal');
+const overlay = document.querySelector('.overlay');
+
+const openModal = function () {
+  modal.classList.remove('hidden');
+  overlay.classList.remove('hidden');
+};
+for (let i = 0; i < openModalBtns.length; i++) {
+  const btn = openModalBtns[i];
+  btn.addEventListener('click', openModal);
+}
+const closeModal = function () {
+  modal.classList.add('hidden');
+  overlay.classList.add('hidden');
+};
+closeModalBtn.addEventListener('click', closeModal);
+overlay.addEventListener('click', closeModal);
+document.addEventListener('keydown', function (e) {
+  if (e.key === 'Escape') {
+    if (!modal.classList.contains('hidden')) {
+      closeModal();
+    }
+  }
+});
