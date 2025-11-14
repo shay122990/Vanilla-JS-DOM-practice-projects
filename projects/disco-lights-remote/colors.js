9d1646bd5aca1def395b42818d8cb3d6a084f02c
shay asanova <moeshakasanova@gmail.com>
2025-11-14 15:43:40 +0400

    gradient colors function with linear transition, created colors.js file

---
+++ projects/disco-lights-remote/colors.js
@@ -0,0 +1,99 @@
+export const multiColors = [
+  '#FF0000',
+  '#FF7F00',
+  '#FFFF00',
+  '#00FF00',
+  '#00FFFF',
+  '#0000FF',
+  '#8B00FF',
+  '#FF00FF',
+  '#00FFAA',
+  '#FF1493',
+  '#FFD700',
+  '#7FFF00',
+  '#40E0D0',
+  '#1E90FF',
+  '#FF4500',
+];
+
+export const bluesColors = [
+  '#001F3F',
+  '#003566',
+  '#004E89',
+  '#0074D9',
+  '#1E90FF',
+  '#00A8E8',
+  '#00BFFF',
+  '#5BC0EB',
+  '#72EDF2',
+  '#46e4f3ff',
+];
+
+export const redsColors = [
+  '#FF0000',
+  '#FF3B30',
+  '#FF4500',
+  '#FF5E57',
+  '#CC0000',
+  '#B30000',
+  '#990000',
+  '#FF1744',
+  '#FF4C4C',
+  '#FFD1D1',
+];
+
+export const neutralColors = [
+  '#FFFFFF',
+  '#F2F2F2',
+  '#E6E6E6',
+  '#CCCCCC',
+  '#B3B3B3',
+  '#999999',
+  '#808080',
+  '#666666',
+  '#4D4D4D',
+  '#333333',
+];
+export const sunsetGradient = [
+  '#FF5F6D',
+  '#FFC371',
+  '#FE8C00',
+  '#F83600',
+  '#FF512F',
+  '#DD2476',
+  '#F857A6',
+  '#FF5858',
+];
+
+export const oceanGradient = [
+  '#2BC0E4',
+  '#EAECC6',
+  '#00C6FB',
+  '#005BEA',
+  '#2E3192',
+  '#1BFFFF',
+  '#00F260',
+  '#0575E6',
+];
+
+export const forestGradient = [
+  '#134E5E',
+  '#71B280',
+  '#5A3F37',
+  '#2C7744',
+  '#007991',
+  '#78FFD6',
+  '#1D976C',
+  '#93F9B9',
+];
+
+export const neonGradient = [
+  '#12c2e9',
+  '#c471ed',
+  '#f64f59',
+  '#f6d365',
+  '#fda085',
+  '#fc00ff',
+  '#00dbde',
+  '#ff9a9e',
+];
--- projects/disco-lights-remote/script.js
+++ projects/disco-lights-remote/script.js
@@ -1,3 +1,16 @@
+'use strict';
+
+import {
+  multiColors,
+  bluesColors,
+  redsColors,
+  neutralColors,
+  sunsetGradient,
+  oceanGradient,
+  forestGradient,
+  neonGradient,
+} from './colors.js';
+
 const powerToggle = document.getElementById('powerToggle');
 
 // solid colors buttons
@@ -5,72 +18,23 @@ const multiBtn = document.getElementById('mode-multi');
 const bluesBtn = document.getElementById('mode-blues');
 const redsBtn = document.getElementById('mode-reds');
 const neutralsBtn = document.getElementById('mode-neutrals');
-
-const multiColors = [
-  '#FF0000',
-  '#FF7F00',
-  '#FFFF00',
-  '#00FF00',
-  '#00FFFF',
-  '#0000FF',
-  '#8B00FF',
-  '#FF00FF',
-  '#00FFAA',
-  '#FF1493',
-  '#FFD700',
-  '#7FFF00',
-  '#40E0D0',
-  '#1E90FF',
-  '#FF4500',
-];
-const bluesColors = [
-  '#001F3F',
-  '#003566',
-  '#004E89',
-  '#0074D9',
-  '#1E90FF',
-  '#00A8E8',
-  '#00BFFF',
-  '#5BC0EB',
-  '#72EDF2',
-  '#CFFAFE',
-];
-const redsColors = [
-  '#FF0000',
-  '#FF3B30',
-  '#FF4500',
-  '#FF5E57',
-  '#CC0000',
-  '#B30000',
-  '#990000',
-  '#FF1744',
-  '#FF4C4C',
-  '#FFD1D1',
-];
-const neutralColors = [
-  '#FFFFFF',
-  '#F2F2F2',
-  '#E6E6E6',
-  '#CCCCCC',
-  '#B3B3B3',
-  '#999999',
-  '#808080',
-  '#666666',
-  '#4D4D4D',
-  '#333333',
-];
+// gradient colors buttons
+const sunsetBtn = document.getElementById('grad-sunset');
+const oceanBtn = document.getElementById('grad-ocean');
+const forestBtn = document.getElementById('grad-forest');
+const neonBtn = document.getElementById('grad-neon');
 
 let powerOn = false;
 let currentInterval = null;
 
-function changeMode(colorArray) {
+function changeSolidMode(colorArray) {
   if (!powerOn) return;
 
   if (currentInterval !== null) {
     clearInterval(currentInterval);
     currentInterval = null;
   }
-
+  document.body.style.transition = 'none';
   let i = 0;
 
   currentInterval = setInterval(() => {
@@ -78,6 +42,23 @@ function changeMode(colorArray) {
     i++;
     if (i >= colorArray.length) i = 0;
   }, 500);
+}
+
+function changeGradientMode(colorArray) {
+  if (!powerOn) return;
+
+  if (currentInterval !== null) {
+    clearInterval(currentInterval);
+    currentInterval = null;
+  }
+  document.body.style.transition = 'background-color 2s linear';
+  let i = 0;
+
+  currentInterval = setInterval(() => {
+    document.body.style.backgroundColor = colorArray[i];
+    i++;
+    if (i >= colorArray.length) i = 0;
+  }, 2000);
 }
 
 powerToggle.addEventListener('click', function () {
@@ -97,7 +78,11 @@ powerToggle.addEventListener('click', function () {
   }
 });
 
-multiBtn.addEventListener('click', () => changeMode(multiColors));
-bluesBtn.addEventListener('click', () => changeMode(bluesColors));
-redsBtn.addEventListener('click', () => changeMode(redsColors));
-neutralsBtn.addEventListener('click', () => changeMode(neutralColors));
+multiBtn.addEventListener('click', () => changeSolidMode(multiColors));
+bluesBtn.addEventListener('click', () => changeSolidMode(bluesColors));
+redsBtn.addEventListener('click', () => changeSolidMode(redsColors));
+neutralsBtn.addEventListener('click', () => changeSolidMode(neutralColors));
+sunsetBtn.addEventListener('click', () => changeGradientMode(sunsetGradient));
+oceanBtn.addEventListener('click', () => changeGradientMode(oceanGradient));
+forestBtn.addEventListener('click', () => changeGradientMode(forestGradient));
+neonBtn.addEventListener('click', () => changeGradientMode(neonGradient));
