fce93409b708f4dabef53b667142812126a820fc
shay asanova <moeshakasanova@gmail.com>
2026-03-25 16:15:52 +0400

    refactored to class app, added event listeners to show form and toggle input change

--- projects/mapty/script.js
+++ projects/mapty/script.js
@@ -11,30 +11,81 @@ const inputDuration = document.querySelector('.form__input--duration');
 const inputCadence = document.querySelector('.form__input--cadence');
 const inputElevation = document.querySelector('.form__input--elevation');
 
-if (navigator.geolocation) {
-  navigator.geolocation.getCurrentPosition(
-    function (position) {
-      // console.log(position); // GeolocationPositioncoords: GeolocationCoordinates {latitude: 42.81757509533469, longitude: 74.62445388754772, altitude: 0, accuracy: 45.75199890136719, altitudeAccuracy: null, …}accuracy: 45.75199890136719latitude: 42.81757509533469longitude: 74.62445388754772[[Prototype]]: GeolocationCoordinatestimestamp: 1774255167380[[Prototype]]: GeolocationPosition
-      const { latitude } = position.coords;
-      const { longitude } = position.coords;
-      // console.log(latitude, longitude); // 42.817645502282 74.6242023796162
-      // console.log(`https://www.google.pt/maps/@${latitude},${longitude}`);
+class App {
+  #map;
+  #mapEvent;
 
-      const coords = [latitude, longitude];
-      const map = L.map('map').setView(coords, 13);
+  constructor() {
+    this._getPosition(); // calling on load
 
-      L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
-        attribution:
-          '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors',
-      }).addTo(map);
+    form.addEventListener('submit', this._newWorkout.bind(this));
 
-      L.marker(coords)
-        .addTo(map)
-        .bindPopup('A pretty CSS popup.<br> Easily customizable.')
-        .openPopup();
-    },
-    function () {
-      alert('Could not get your position');
-    },
-  );
+    inputType.addEventListener('change', this._toggleElevationField);
+  }
+  _getPosition() {
+    if (navigator.geolocation) {
+      navigator.geolocation.getCurrentPosition(
+        this._loadMap.bind(this),
+        function () {
+          alert('Could not get your position');
+        },
+      );
+    }
+  }
+  _loadMap(position) {
+    // console.log(position); // GeolocationPositioncoords: GeolocationCoordinates {latitude: 42.81757509533469, longitude: 74.62445388754772, altitude: 0, accuracy: 45.75199890136719, altitudeAccuracy: null, …}accuracy: 45.75199890136719latitude: 42.81757509533469longitude: 74.62445388754772[[Prototype]]: GeolocationCoordinatestimestamp: 1774255167380[[Prototype]]: GeolocationPosition
+    const { latitude } = position.coords;
+    const { longitude } = position.coords;
+    // console.log(latitude, longitude); // 42.817645502282 74.6242023796162
+    // console.log(`https://www.google.pt/maps/@${latitude},${longitude}`);
+
+    const coords = [latitude, longitude];
+
+    this.#map = L.map('map').setView(coords, 13); // map is the id of the div in html
+
+    //console.log(map); // e {options: {…}, _handlers: Array(7), _layers: {…}, _zoomBoundLayers: {…}, _sizeChanged: false, …}
+
+    L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
+      attribution:
+        '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors',
+    }).addTo(this.#map);
+
+    // handle clicks on a map
+    this.#map.on('click', this._showForm.bind(this));
+  }
+  _showForm(mapE) {
+    this.#mapEvent = mapE;
+    form.classList.remove('hidden');
+    inputDistance.focus();
+  }
+  _toggleElevationField() {
+    inputElevation.closest('.form__row').classList.toggle('form__row--hidden');
+    inputCadence.closest('.form__row').classList.toggle('form__row--hidden');
+  }
+  _newWorkout(e) {
+    e.preventDefault();
+
+    inputDistance.value =
+      inputDuration.value =
+      inputCadence.value =
+      inputElevation.value =
+        '';
+    //display marker
+    const { lat, lng } = this.#mapEvent.latlng;
+    L.marker([lat, lng])
+      .addTo(this.#map)
+      .bindPopup(
+        L.popup({
+          maxWidth: 250,
+          minWidth: 100,
+          autoClose: false,
+          closeOnClick: false,
+          className: 'running-popup',
+        }),
+      )
+      .setPopupContent('workout')
+      .openPopup();
+  }
 }
+
+const app = new App();
